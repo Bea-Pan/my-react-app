3e54f122088ba00840f32bf77b67bb33ce9a456e
Bea Pan <bpankowska@gmail.com>
2023-06-01 16:23:47 +0200

    next sentece

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Please work this time!!
+          Please work this time!! Updated sentence.
         </p>
         <a
           className="App-link"
